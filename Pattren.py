8a613edfadb502dcfbc7732ca8374a2d5e2c3615
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-13 23:37:06 +0530

    updating problems

--- Pattren.py
+++ Pattren.py
@@ -18,4 +18,11 @@
 
 
 # 3. Pyramid Pattern
+# n = int(input("Enter number of rows: "))
+
+# for i in range(n):
+#     spaces = " " * (n - i - 1)
+#     stars = "*" * (2 * i + 1)
+#     print(spaces + stars)
+
 
